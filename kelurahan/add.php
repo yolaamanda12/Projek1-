97ae75a39822e89cf4adcfee81b7375a80ac677b
yolaamanda12 <147458765+yolaamanda12@users.noreply.github.com>
2024-05-11 08:08:12 -0700

    Add files via upload

---
+++ kelurahan/add.php
@@ -0,0 +1,90 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+
+require '../dbkoneksi.php';
+if (isset($_POST["submit"])) {
+    $_nama = $_POST['nama'];
+    $_kec_id = $_POST['kec_id'];
+    $data = [$_nama, $_kec_id];
+    $sql = "INSERT INTO kelurahan (nama, kec_id) VALUES (?, ?)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+    echo "<script>window.location.href = 'index.php';</script>";
+}
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard Website</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Form kelurahan</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h2 class="text-center">Form Kelurahan</h2>
+                            <form action="add.php" method="POST">
+                                <div class="form-group row">
+                                    <label for="nama" class="col-4 col-form-label">Nama</label>
+                                    <div class="col-8">
+                                        <input id="nama" name="nama" type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="kec_id" class="col-4 col-form-label">Kecamatan ID</label>
+                                    <div class="col-8">
+                                        <input id="kec_id" name="kec_id" type="text" class="form-control">
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Projek 1 - Aplikasi Web Sederhana Puskesmas
+                        </div>
+                        <!-- /.card-footer-->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
---
+++ periksa/add.php
@@ -0,0 +1,148 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+
+require '../dbkoneksi.php';
+if (isset($_POST["submit"])) {
+    $_tanggal = $_POST['tanggal'];
+    $_berat = $_POST['berat'];
+    $_tinggi = $_POST['tinggi'];
+    $_tensi = $_POST['tensi'];
+    $_keterangan = $_POST['keterangan'];
+    $_pasien_id = $_POST['pasien_id'];
+    $_dokter_id = $_POST['dokter_id'];
+    $data = [$_tanggal, $_berat, $_tinggi, $_tensi, $_keterangan, $_pasien_id, $_dokter_id];
+    $sql = "INSERT INTO periksa (tanggal, berat, tinggi, tensi, keterangan, pasien_id, dokter_id) VALUES (?, ? ,? ,? ,? , ?, ?)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+    echo "<script>window.location.href = 'index.php';</script>";
+}
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard Website</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Form Periksa</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h2 class="text-center">Form Periksa</h2>
+                            <form action="add.php" method="POST">
+                                <div class="form-group row">
+                                    <label for="tanggal" class="col-4 col-form-label">Tanggal Periksa</label>
+                                    <div class="col-8">
+                                        <input id="tanggal" name="tanggal" type="date" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="berat" class="col-4 col-form-label">Berat Badan</label>
+                                    <div class="col-8">
+                                        <input id="berat" name="berat" type="number" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="tinggi" class="col-4 col-form-label">Tinggi Badan</label>
+                                    <div class="col-8">
+                                        <input id="tinggi" name="tinggi" type="number" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="tensi" class="col-4 col-form-label">Tensi</label>
+                                    <div class="col-8">
+                                        <input id="tensi" name="tensi" type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="keterangan" class="col-4 col-form-label">Keterangan Periksa</label>
+                                    <div class="col-8">
+                                        <textarea name="keterangan" id="keterangan" cols="30" rows="5" class="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="pasien_id" class="col-4 col-form-label">Pasien ID</label>
+                                    <div class="col-8">
+                                        <?php
+                                        $sqljenis = "SELECT * FROM pasien";
+                                        $rsjenis = $dbh->query($sqljenis);
+                                        ?>
+                                        <select id="pasien_id" name="pasien_id" class="custom-select">
+                                            <?php
+                                            foreach ($rsjenis as $rowjenis) {
+                                            ?>
+                                                <option value="<?= $rowjenis['id'] ?>"><?= $rowjenis['kode'] ?></option>
+                                            <?php
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="pasien_id" class="col-4 col-form-label">Dokter ID</label>
+                                    <div class="col-8">
+                                        <?php
+                                        $sqljenis = "SELECT * FROM dokter";
+                                        $rsjenis = $dbh->query($sqljenis);
+                                        ?>
+                                        <select id="dokter_id" name="dokter_id" class="custom-select">
+                                            <?php
+                                            foreach ($rsjenis as $rowjenis) {
+                                            ?>
+                                                <option value="<?= $rowjenis['id'] ?>"><?= $rowjenis['nama'] ?></option>
+                                            <?php
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Footer
+                        </div>
+                        <!-- /.card-footer-->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
